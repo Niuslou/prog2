30249081f4e1fc34e7d27512f1742b2eb402c5d7
Niuslou <129942769+Niuslou@users.noreply.github.com>
2025-01-22 11:41:41 +0100

    Einleitung zum Sport

--- unihockey-website/src/App.js
+++ unihockey-website/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <h1 className="title">Titel</h1>
+        <h1 className="title">Unihockey</h1>
         <nav className="nav">
           <span>Home</span>
           <input type="text" placeholder="Suchen" className="search-bar" />
@@ -19,7 +19,7 @@ function App() {
           <div className="intro-image"></div>
           <div className="intro-text">
             <h2>Kurze Einleitung zu Unihockey</h2>
-            <p>dgfsdasdhrQJTURZRHDJDUZEHDFDFUTURDHGKFDHKFJDFH</p>
+            <p>Unihockey, auch als Floorball bekannt, ist eine schnelle und dynamische Hallensportart, die Ähnlichkeiten mit Eishockey hat, jedoch ohne Schlittschuhe gespielt wird. Die Spieler nutzen leichte Schläger aus Kunststoff und einen hohlen Plastikball mit Löchern, um Tore zu erzielen. Gespielt wird in der Regel auf einem Spielfeld mit Banden, das etwa die Größe eines Handballfeldes hat. Jede Mannschaft besteht aus fünf Feldspielern und einem Torhüter.Das Spiel ist geprägt von Schnelligkeit, Präzision und Teamarbeit. Körperkontakt ist im Vergleich zu anderen Hockeysportarten stark eingeschränkt, was Unihockey besonders fair und zugänglich macht. Die Sportart hat ihre Wurzeln in Schweden und wird heute weltweit gespielt, mit offiziellen Weltmeisterschaften und wachsender Popularität, insbesondere in Europa.Unihockey eignet sich sowohl für Anfänger als auch für Profis und wird oft in Schulen, Vereinen oder Freizeiteinrichtungen gespielt. Es ist ein Sport, der Fitness, Koordination und strategisches Denken fördert und gleichzeitig großen Spaß macht!</p>
           </div>
         </div>
         
